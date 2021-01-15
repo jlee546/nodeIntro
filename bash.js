2c07f511d64d45263f694c2af62ba95f88015c7c
Jessica Lee <jessicalee546@gmail.com>
2021-01-15 15:35:42 -0500

    created ls.js

--- bash.js
+++ bash.js
@@ -1,10 +1,17 @@
+const pwdFunc = require('./pwd')
+const lsFunc = require('./ls')
+
 process.stdout.write('prompt > ');
 
 process.stdin.on('data',(data)=>{
     const cmd=data.toString().trim();
     if(cmd==='pwd'){
-        process.stdout.write(__filename);
-    }else{
+        pwdFunc()
+    }
+    else if (cmd==='ls'){
+      lsFunc()
+    }
+     else{
         process.stdout.write('You typed: '+cmd);
     }
     process.stdout.write('\nprompt> ');
---
+++ pwd.js
@@ -0,0 +1,4 @@
+module.exports = function pwdFunction(){
+  process.stdout.write(__filename);
+}
+
